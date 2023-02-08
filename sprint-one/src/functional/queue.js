920fc3973d246aee587c8a5c1d697b482a168bec
Nam Nguyen <nam.nguyen201@gmail.com>
2023-02-08 15:08:01 -0800

    Pass tests in functional queue.js and functional stack.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,39 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //goal: add the value with the correct key to the storage
+
+    //get the array of keys
+    var storageKeys = Object.keys(storage);
+    //find the value of the first element which becomes the name of the current key
+    var currentKey = storageKeys[0];
+    //create the object key from (currentKey + 1) and a value of the argument value
+    storage[currentKey + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    //goal: remove the first property in the object (first in first out)
+
+    //get the array of keys
+    var keys = Object.keys(storage); //[1, 2, 3]
+    //get the value of the first element of the array of keys
+    var firstKey = keys[0]; //1
+    //save the value of the first key in a variable
+    var firstValue = storage[firstKey]; //a
+
+    //delete the key-value property with the name of the first element from array of keys
+    delete storage[firstKey]; //storage[1]
+    return firstValue;
   };
 
   someInstance.size = function() {
+    //get the array of keys
+    var keys = Object.keys(storage);
+    //get the length of the array of keys
+    var keysLength = keys.length;
+
+    //return the length
+    return keysLength;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,34 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //check the length of someValue
+    var storageKeys = Object.keys(storage);
+    var storageLength = storageKeys.length;
+    //assign key to be the length + 1 and the value to be someValue
+    storage[storageLength + 1] = value;
+    //storage[key] = someValue
   };
 
   someInstance.pop = function() {
+    //get an array of keys
+    var storageKeys = Object.keys(storage);
+    //get the lengh of the array of keys
+    var storageLength = storageKeys.length;
+    //create a variable for the value of the last property
+    var lastItem = storage[storageLength];
+    //delete the last property
+    delete storage[storageLength];
+    //return the value of the last property
+    return lastItem;
+
+
   };
 
   someInstance.size = function() {
+    //get the length of the instance keys
+    var keys = Object.keys(storage);
+    //return the value of that length
+    return keys.length;
   };
 
   return someInstance;
