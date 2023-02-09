0f8ffbe9f179be7ec63a6d990c9c00fadc25a5e9
Nam Nguyen <nam.nguyen201@gmail.com>
2023-02-09 12:27:22 -0800

    Implement stack and queue in es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -4,4 +4,37 @@ class Queue {
   constructor() {
   }
 
+  size() {
+    var keys = Object.keys(this);
+
+    return keys.length;
+  }
+
+  enqueue(string) {
+    //get array of keys
+    var keys = Object.keys(this);
+    //get the last element
+    var lastElement = keys[keys.length - 1];
+    //create an object key with that (element + 1) and a value of string
+    this[lastElement + 1] = string;
+
+    //return length of array of keys
+    return keys.length;
+  }
+
+  dequeue() {
+    //get array of keys
+    var keys = Object.keys(this);
+    //get first element
+    var firstElement = keys[0];
+    //save the value of the key that matches the first element
+    var value = this[firstElement];
+    //delete the key that matches the first element
+    delete this[firstElement];
+    //return the saved valu
+    return value;
+  }
+
 }
+
+var Final = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -4,4 +4,38 @@ class Stack {
   constructor() {
   }
 
+  size() {
+    //get array of keys
+    var keys = Object.keys(this);
+    //return length of the array
+    return keys.length;
+  }
+
+  push(string) {
+    //get array of keys
+    var keys = Object.keys(this);
+    //get the last element in the array
+    var lastElement = keys[keys.length - 1];
+
+    //create a key with (last element + 1) and a value of string
+    this[lastElement + 1] = string;
+    //return array length
+    return keys.length;
+  }
+
+  pop() {
+    //get array of keys
+    var keys = Object.keys(this);
+    //get the last element in the array
+    var lastElement = keys[keys.length - 1];
+    //save the value of the key that matches the last element from the array
+    var value = this[lastElement];
+    //delete that key-value pair
+    delete this[lastElement];
+    //return saved value
+    return value;
+  }
+
 }
+
+var Final = new Stack();
